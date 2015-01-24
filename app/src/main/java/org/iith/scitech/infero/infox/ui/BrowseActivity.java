f449e9faa25ef724e3471deb8673eb490c3d61d2
shashank-jaiswal-2013 <me12b1033@iith.ac.in>
2015-01-24 17:01:30 +0530

    Added server folder

--- app/src/main/java/org/iith/scitech/infero/infox/ui/BrowseActivity.java
+++ app/src/main/java/org/iith/scitech/infero/infox/ui/BrowseActivity.java
@@ -137,6 +137,24 @@ public class BrowseActivity extends ActionBarActivity
             try
             {
                 Thread.sleep(4000);
+                /*
+
+                String url = "url you want to download";
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setDescription("Some descrition");
+                request.setTitle("Some title");
+                // in order for this if to run, you must use the android 3.2 to compile your app
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                }
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "name-of-the-file.ext");
+
+                // get download service and enqueue file
+                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                manager.enqueue(request);
+
+                * */
             }
             catch (InterruptedException e) {
             }
@@ -195,7 +213,8 @@ public class BrowseActivity extends ActionBarActivity
 
     private void addTile(final Activity context, LinearLayout.LayoutParams params, int i, final String tileType, final String data)
     {
-        tileViewGroup = (LinearLayout) context.findViewById(R.id.contentLinearLayout);
+        //tileViewGroup = (LinearLayout) context.findViewById(R.id.contentLinearLayout);
+
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         switch (tileType)
         {
---
+++ app/src/main/java/org/iith/scitech/infero/infox/ui/ContentListAdapter.java
@@ -0,0 +1,49 @@
+package org.iith.scitech.infero.infox.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.iith.scitech.infero.infox.R;
+
+/**
+ * Created by shashank on 24/1/15.
+ */
+
+public class ContentListAdapter extends BaseAdapter
+{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 5;
+    }
+}
