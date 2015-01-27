0ca2bffd04a97c43ae5d76b9d9fb36024d9e225f
Agam Agarwal <agammaster@yahoo.co.in>
2015-01-28 01:11:48 +0530

    download.php: send content according to preferences of the user

--- Server/download.php
+++ Server/download.php
@@ -2,8 +2,25 @@
 require_once("common/constants.php");
 require_once("connect_database.php");
 
+if(!isset($_POST['data']))
+	die(INVALID_REQUEST);
+
+$json_data=json_decode($_POST['data'], true);
+
+$phone=mysqli_real_escape_string($json_data["phNo"]);
+
+$query="SELECT user_id FROM users WHERE phone='$phone'";
+$result=mysqli_query($connection, $query);
+
+if(!$result)
+	die(INVALID_REQUEST);
+
+$user_id=mysqli_fetch_assoc($result)["user_id"];
+
 //TODO: Modify this to get apt content
-$query="SELECT * FROM contents ORDER BY rand();";
+$query="SELECT * FROM contents AS c
+	INNER JOIN preferences AS p ON (c.category_id=p.category_id AND p.user_id='$user_id')
+	ORDER BY rand() LIMIT 5;";
 
 $result=mysqli_query($connection, $query);
 
