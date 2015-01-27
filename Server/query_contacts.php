78068c27fdb965201b256d2e9003279c0921525b
Agam Agarwal <agammaster@yahoo.co.in>
2015-01-28 00:47:20 +0530

    fixed bug in query_contacts.php

--- Server/query_contacts.php
+++ Server/query_contacts.php
@@ -15,20 +15,18 @@
 
       $json = json_decode($contact_json_string, true);
 
+      $data = $json["data"];
       $registered_contacts = array();
-      foreach ($json as $item)
+      foreach($data as $contact)
       {
-         foreach($item->data->number as $contact)
-         {
-            $sql_query = "SELECT * FROM `users` WHERE `phone` = '$contact'";
-            $result = mysqli_query($connection, $sql_query);
-         
-            /* error in connection. */
-            if(!$result)
-               die("Error building contact list: " . mysqli_error($connection));
-            if(mysqli_num_rows($result)==1)
-               array_push($registered_contacts, $contact);
-         }
+         $sql_query = "SELECT * FROM `users` WHERE `phone` = '".$contact["number"]."'";
+         $result = mysqli_query($connection, $sql_query);
+      
+         /* error in connection. */
+         if(!$result)
+            die("Error building contact list: " . mysqli_error($connection));
+         if(mysqli_num_rows($result)==1)
+            array_push($registered_contacts, $contact);
       }
       echo json_encode($registered_contacts);
    }
