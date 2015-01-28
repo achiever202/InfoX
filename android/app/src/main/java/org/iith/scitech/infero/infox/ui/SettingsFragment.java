289d4ef7c30307a31f535d70b3fb2a656a611f7a
shashank-jaiswal-2013 <me12b1033@iith.ac.in>
2015-01-29 03:42:21 +0530

    Create content preferences multichoice dialog box

--- android/app/src/main/java/org/iith/scitech/infero/infox/ui/SettingsFragment.java
+++ android/app/src/main/java/org/iith/scitech/infero/infox/ui/SettingsFragment.java
@@ -1,12 +1,15 @@
 package org.iith.scitech.infero.infox.ui;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -15,6 +18,11 @@ import android.widget.TextView;
 
 import org.iith.scitech.infero.infox.R;
 import org.iith.scitech.infero.infox.util.PrefUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -81,11 +89,55 @@ public class SettingsFragment extends Fragment {
         prefTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog dialog = new Dialog(getActivity());
+                /*Dialog dialog = new Dialog(getActivity());
                 dialog.setTitle("Preferences");
                 dialog.setContentView(R.layout.dialog_preferences);
                 ListView lw = (ListView) dialog.findViewById(R.id.pref_contentPreferences_listView);
-                
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.pref_contentTypes_entries, android.R.layout.simple_list_item_multiple_choice);
+                lw.setAdapter(adapter);
+                dialog.show();*/
+
+                final List<Integer> mSelectedItems = new ArrayList();  // Where we track the selected items
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                builder.setTitle("Preferences")
+                        // Specify the list array, the items to be selected by default (null for none),
+                        // and the listener through which to receive callbacks when items are selected
+                        .setMultiChoiceItems(R.array.pref_contentTypes_entries, null,
+                                new DialogInterface.OnMultiChoiceClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which,
+                                                        boolean isChecked) {
+                                        if (isChecked) {
+                                            // If the user checked the item, add it to the selected items
+                                            mSelectedItems.add(which);
+                                        } else if (mSelectedItems.contains(which)) {
+                                            // Else, if the item is already in the array, remove it
+                                            mSelectedItems.remove(Integer.valueOf(which));
+                                        }
+                                    }
+                                })
+                                // Set the action buttons
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK, so save the mSelectedItems results somewhere
+                                // or return them to the component that opened the dialog
+                                JSONArray jsonArray = new JSONArray();
+                                for (int i = 0; i < mSelectedItems.size(); i++) {
+                                    jsonArray.put(getResources().getStringArray(R.array.pref_contentTypes_values)[mSelectedItems.get(i)]);
+                                }
+                                PrefUtils.setContentPreferences(getActivity(), jsonArray.toString());
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                builder.create();
+                builder.show();
             }
         });
 
