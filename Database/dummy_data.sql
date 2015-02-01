f6e4297e81b5d8ced205369e884afca2e6282ddb
Agam Agarwal <agammaster@yahoo.co.in>
2015-02-01 10:55:55 +0530

    modified dummy content data

--- Database/dummy_data.sql
+++ Database/dummy_data.sql
@@ -25,9 +25,8 @@ VALUES
 INSERT INTO contents
 	(content, file_name, file_path, time_expiry, lang_id, category_id, content_type_id)
 VALUES
-	("Baby doll mai sone di", NULL, NULL, NULL, "EN", "EDU", "TXT")
-	,("24;7:00 AM;26-01-2015", NULL, NULL, NULL, "EN", "PS", "TXT")
-	,(NULL, "Airtel - Har Ek Friend Zaroori Hota Hai.mp4", "content/hefzhh.mp4", NULL, "HI", "ENT", "VID")
+	("Test educational content. Test educational content. Test educational content.", NULL, NULL, NULL, "EN", "EDU", "TXT")
+	,("24;7:00 AM;01-02-2015", NULL, NULL, NULL, "EN", "PS", "TXT")
 	;
 
 INSERT INTO users
